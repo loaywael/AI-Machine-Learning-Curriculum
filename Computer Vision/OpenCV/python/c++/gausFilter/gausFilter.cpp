f2d91583b47b4aca16d600b3510565ebbbc14a08
loaywael <hythamconn1@gmail.com>
2019-12-18 15:28:01 +0200

    gaussian blur c++ opencv

---
+++ Computer Vision/OpenCV/python/c++/gausFilter/gausFilter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+int main(int argc, char** argv) {
+    cv::Mat img;
+    cv::Mat gauFtr;
+    img = cv::imread("tomato1.jpeg", 1);
+    cv::GaussianBlur(img, gauFtr, cv::Size(5, 5), 0, 0);
+    cv::namedWindow("frame", cv::WINDOW_AUTOSIZE);
+    cv::imshow("frame", img);
+    cv::waitKey(0);
+    cv::imshow("frame", gauFtr);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+    return 0;
+}
