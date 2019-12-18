7285c314a44f03a015a58416863318309fdc60d8
loaywael <hythamconn1@gmail.com>
2019-12-18 16:13:04 +0200

    down samplying C++ OpenCV

--- Computer Vision/OpenCV/c++/gausFilter/gausFilter.cpp
+++ Computer Vision/OpenCV/c++/gausFilter/gausFilter.cpp
@@ -5,8 +5,10 @@
 int main(int argc, char** argv) {
     cv::Mat img;
     cv::Mat gauFtr;
+    cv::Mat downSampledImg;
     img = cv::imread("tomato1.jpeg", 1);
     cv::GaussianBlur(img, gauFtr, cv::Size(5, 5), 0, 0);
+    cv::pyrDown(gauFtr, downSampledImg);
     cv::namedWindow("frame", cv::WINDOW_AUTOSIZE);
     cv::imshow("frame", img);
     cv::waitKey(0);
