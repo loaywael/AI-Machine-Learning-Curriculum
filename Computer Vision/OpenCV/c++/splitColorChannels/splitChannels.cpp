c3fe2de43d7b7844fc7764d1124ff1a53d473d8c
loaywael <hythamconn1@gmail.com>
2019-12-26 23:38:01 +0200

    seprating RGB color-channels

---
+++ Computer Vision/OpenCV/c++/splitColorChannels/splitChannels.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv) {
+
+    cv:: Mat img, channels[3];
+
+    img = cv::imread("tomato1.jpeg", cv::IMREAD_COLOR);
+    cv::split(img, channels);
+
+    cv::namedWindow("blue-channel", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("green-channel", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("red-channel", cv::WINDOW_AUTOSIZE);
+
+    cv::moveWindow("blue-channel", 500, 350);
+    cv::moveWindow("green-channel", img.cols + 600, 350);
+    cv::moveWindow("red-channel", img.cols * 2 + 700, 350);
+
+    cv::imshow("blue-channel", channels[0]);
+    cv::imshow("green-channel", channels[1]);
+    cv::imshow("red-channel", channels[2]);
+
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+
+    return 0;
+}
