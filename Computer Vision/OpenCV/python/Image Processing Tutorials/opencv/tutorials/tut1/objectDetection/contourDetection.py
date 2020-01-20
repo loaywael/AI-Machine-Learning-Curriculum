280bac3f094645dcc89f1f3e4075fc3346d43a70
loaywael <hythamconn1@gmail.com>
2020-01-20 23:54:40 +0200

    contour detection

--- Computer Vision/OpenCV/python/Image Processing Tutorials/opencv/tutorials/tut1/objectDetection/contourDetection.py
+++ Computer Vision/OpenCV/python/Image Processing Tutorials/opencv/tutorials/tut1/objectDetection/contourDetection.py
@@ -3,24 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-objects = cv2.imread("../../../gallery/objects.jpg", cv2.IMREAD_COLOR)
-grayObjs = cv2.cvtColor(objects, cv2.COLOR_BGR2GRAY)
+pennyImg = cv2.imread("../../../gallery/penny.jpg", cv2.IMREAD_COLOR)
+pennyImg = cv2.resize(pennyImg, None, fx=0.5, fy=0.5)
+grayPenny = cv2.cvtColor(pennyImg, cv2.COLOR_BGR2GRAY)
+grayPenny = cv2.medianBlur(grayPenny, 9)
+_, mask = cv2.threshold(grayPenny, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
 
-mask = cv2.adaptiveThreshold(
-    grayObjs, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 125, 1)
-img, hierarchy = cv2.findContours(grayObjs, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-print(img)
-# compelete_contours: the number of found contours in the image
-# hierarchy: the point which draws the contours
-# internal & external shapes found in the image
+contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(pennyImg, contours, -1, (255, 0, 0), 3)
 
-# contMask = np.zeros_like(mask)
-# for i in range(len(compContours)):
-#     if hierarchy[0][i][3] == -1:     # external contour
-#         cv2.drawContours(contMask, compContours, i, (255, 0, 0), -1)
 
-cv2.imshow("img", img)
-# cv2.imshow("FG", contMask)
-# cv2.imshow("BKG", imgBKG)
+cv2.imshow("mask", pennyImg)
+cv2.imshow("ranged-frame", grayPenny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ Computer Vision/OpenCV/python/Image Processing Tutorials/opencv/tutorials/tut1/objectDetection/imgSegmentation.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+pennyImg = cv2.imread("../../../gallery/penny.jpg", cv2.IMREAD_COLOR)
+pennyImg = cv2.resize(pennyImg, None, fx=0.5, fy=0.5)
+grayPenny = cv2.cvtColor(pennyImg, cv2.COLOR_BGR2GRAY)
+grayPenny = cv2.medianBlur(grayPenny, 15)
+_, mask = cv2.threshold(grayPenny, 140, 255, cv2.THRESH_BINARY_INV)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
+
+contours, hierarchy = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(pennyImg, contours, -1, (255, 0, 0), 3)
+
+
+cv2.imshow("mask", pennyImg)
+cv2.imshow("ranged-frame", grayPenny)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
