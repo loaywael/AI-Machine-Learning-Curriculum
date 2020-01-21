f76f3fe5a426d0eb8f0c389da7ec830394fadd45
loaywael <hythamconn1@gmail.com>
2020-01-21 22:27:19 +0200

    watershed & image segmentation

--- Computer Vision/OpenCV/python/Image Processing Tutorials/opencv/tutorials/tut1/objectDetection/contourDetection.py
+++ Computer Vision/OpenCV/python/Image Processing Tutorials/opencv/tutorials/tut1/objectDetection/contourDetection.py
@@ -10,11 +10,14 @@ grayPenny = cv2.medianBlur(grayPenny, 9)
 _, mask = cv2.threshold(grayPenny, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))
 
+# distTrans = cv2.distanceTransform(mask, cv2.DIST_L2, 5)
+# _, mask = cv2.threshold(distTrans, 0.7*distTrans.max(), 255, 0)
+
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(pennyImg, contours, -1, (255, 0, 0), 3)
 
 
 cv2.imshow("mask", pennyImg)
-cv2.imshow("ranged-frame", grayPenny)
+cv2.imshow("ranged-frame", mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
